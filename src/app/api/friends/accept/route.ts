6807b485e5d1642f7fb6e719463e72928a2177ff
roshan-jha-23 <jha062305@gmail.com>
2024-06-22 12:20:14 +0530

    accept frnd req route

---
+++ src/app/api/friends/accept/route.ts
@@ -0,0 +1,44 @@
+import { fetchRedis } from "@/helpers/redis";
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/redis.";
+import { getServerSession } from "next-auth";
+import { z } from "zod";
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { id: idToAdd } = z.object({ id: z.string() }).parse(body);
+
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+    const isAlreadyFriends = await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:friends`,
+      idToAdd
+    );
+
+    if (isAlreadyFriends) {
+      return new Response("Already friends", { status: 400 });
+    }
+
+    const hasFriendRequest = await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:incoming_friend_request`,
+      idToAdd
+    );
+    if(!hasFriendRequest){
+      return new Response('No friend Request ',{status:400})
+    }
+    await db.sadd(`user:${session.user.id}:friends`, idToAdd);
+    await db.sadd(`user:${idToAdd}:friends`, session.user.id);
+    await db.srem(`user:${idToAdd}:incoming_friend_request`, session.user.id);
+    await db.srem(`user:${session.user.id}:incoming_friend_request`, idToAdd);
+    return new Response('Your Freind has been sent',{status:200})
+  } catch (error) {
+       if(error instanceof z.ZodArray){
+        return new Response(`Invalid request Payload`,{status:422})
+       }
+       return new Response('invalid request',{status:400})
+  }
+}
--- src/app/components/FriendRequests.tsx
+++ src/app/components/FriendRequests.tsx
@@ -1,5 +1,7 @@
 "use client";
+import axios from "axios";
 import { Check, UserPlus, X } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { FC, useState } from "react";
 
 interface FriendRequestsProps {
@@ -14,6 +16,21 @@ const FriendRequests: FC<FriendRequestsProps> = ({
   const [friendRequests, setFriendRequests] = useState<IncomingFriendRequest>(
     incomingFriendRequest
   );
+  const router = useRouter();
+  const acceptFriend = async (senderId: string) => {
+    await axios.post("/api/friends/accept", { id: senderId });
+    setFriendRequests((prev) =>
+      prev.filter((request) => request.senderId !== senderId)
+    );
+    router.refresh();
+  };
+  const denyFriend = async (senderId: string) => {
+    await axios.post("/api/friends/deny", { id: senderId });
+    setFriendRequests((prev) =>
+      prev.filter((request) => request.senderId !== senderId)
+    );
+    router.refresh();
+  };
   return (
     <>
       {friendRequests.length === 0 ? (
@@ -22,7 +39,7 @@ const FriendRequests: FC<FriendRequestsProps> = ({
         friendRequests.map((request) => (
           <div key={request.senderId} className="flex gap-4 items-center">
             <UserPlus className="text-black" />
-            <p className="font-medium text-lg">{request.senderEmail}</p>
+            <p className="font-medium text-lg">{request.senderName}</p>
             <button
               onClick={() => acceptFriend(request.senderId)}
               aria-label="accept friend"
